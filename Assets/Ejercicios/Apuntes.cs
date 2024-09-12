237e7b06f1da9e9bc9a1a264f2691e559fe791eb
Serchk <sergioperezalcal@gmail.com>
2024-09-12 19:20:05 +0200

    apuntes

--- Assets/Ejercicios/Apuntes.cs
+++ Assets/Ejercicios/Apuntes.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Apuntes : MonoBehaviour
 {
+    // Variables GLOBALES
+    int numeroA;
+    int numeroB;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // mantener control + clic izq para ir al método
+        int compraEnCasa = SumarDosPagas(numeroA, numeroB); // puedes llamar variables globales para representar las locales del método
+        Debug.Log(compraEnCasa);
+        Debug.Log(SumarDosPagas(numeroA, numeroB));// no hay por qué crear nueva variable en este caso
     }
+
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    int SumarDosPagas(int paga1, int paga2) // escribir el dato entre parentesis y llamar al valor en donde llames al método
+    {
+        // Variables LOCALES
+        //int paga1 = 3;
+        //int paga2 = 5;
+        int compra = paga1 + paga2;
+        return compra;
     }
 }
