468cce334ec1a38d736367d857ddadd1e03c8e28
Serchk <sergioperezalcal@gmail.com>
2024-09-13 12:25:48 +0200

    ejercicio 2.3

---
+++ Assets/Ejercicios/Ejercicio2_3.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio2_3 : MonoBehaviour
+{
+    int vidas = 20, dobleDeVidas;
+    float exp = 15, tripleDeExp;
+    int producto;
+    float cociente, resto;
+    // Start is called before the first frame update
+    void Start()
+    {
+       producto = vidas * exp;
+       cociente = vidas / exp;
+       resto = vidas % exp;
+       dobleDeVidas = vidas * 2;
+       tripleDeExp = exp * 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
