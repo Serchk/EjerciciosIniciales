45077fad6b4b9038bf36f3d7d583e45b8d1e28bd
Serchk <sergioperezalcal@gmail.com>
2024-09-16 12:40:58 +0200

    ej 2.6

--- Assets/Ejercicios/Ejercicio2_6.cs
+++ Assets/Ejercicios/Ejercicio2_6.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Ejercicio2_6 : MonoBehaviour
 {
+    int vida = 10, poder = 40, escudo = 50;
+    int sumaValores, media, cantidadDeValores = 3;
     // Start is called before the first frame update
     void Start()
     {
-        
+        sumaValores = vida + poder + escudo;
+        media = sumaValores / cantidadDeValores;
+        Debug.Log("Vida: " + vida + " || Poder: " + poder + " || Escudo: " + escudo + " || Media: " + media);
     }
 
     // Update is called once per frame
