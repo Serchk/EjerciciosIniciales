44ec936b3552d8a470c10649b70cb7e915af1154
Serchk <sergioperezalcal@gmail.com>
2024-09-18 17:48:50 +0200

    apuntes for

--- Assets/Ejercicios/ApuntesFor.cs
+++ Assets/Ejercicios/ApuntesFor.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class ApuntesFor : MonoBehaviour
 {
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //While -> INDETERMINADO: no contar  
+
+        //for - > DTERMINADO: contar
+        for (int i = 0; i < 89; i++) //for (inicio; condición; paso)
+        {
+
+        }
     }
 
     // Update is called once per frame
