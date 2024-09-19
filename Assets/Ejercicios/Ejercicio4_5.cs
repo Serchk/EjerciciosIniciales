b5307e2acb20f1fa3d44bd93b43f9db5e618172a
Serchk <sergioperezalcal@gmail.com>
2024-09-19 12:32:43 +0200

    up

--- Assets/Ejercicios/Ejercicio4_5.cs
+++ Assets/Ejercicios/Ejercicio4_5.cs
@@ -30,18 +30,18 @@ public class Ejercicio4_5 : MonoBehaviour
         if (moneda == "euro")
         {
             float convertirADolar = cantidad * 1.1117866f;
-            string conversion = "4.3 || " + cantidad + " euros son " + convertirADolar + " dolares.";
+            string conversion = "4.5 || " + cantidad + " euros son " + convertirADolar + " dolares.";
             return conversion;
         }
         else if (moneda == "dolares")
         {
             float convertirAEuro = cantidad * 0.90009557f;
-            string conversion = "4.3 || " + cantidad + "dolares son " + convertirAEuro + " euros.";
+            string conversion = "4.5 || " + cantidad + "dolares son " + convertirAEuro + " euros.";
             return conversion;
         }
         else
         {
-            string conversion = "4.3 || Indica la moneda para poder hacer la conversión(euro o dolar)";
+            string conversion = "4.5 || Indica la moneda para poder hacer la conversión(euro o dolar)";
             return conversion;
         }
     }
