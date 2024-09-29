9b45fc31657af3d004bd08b526b41af59f5c173c
Serchk <sergioperezalcal@gmail.com>
2024-09-29 20:53:19 +0200

    ej 7.12 no conseguido

---
+++ Assets/Ejercicios/Ej 7/Ejercicio7_12.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio7_12 : MonoBehaviour
+{
+    //Nolo consigohacer
+
+    //[SerializeField] int numero1, numero2;
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    int resultado = SumarTodosLosNumeros(numero1, numero2);
+    //    Debug.Log("La suma de todos los números entre " + numero1 + " y " + numero2 + " es igual a " + resultado);
+    //}
+
+    //int SumarTodosLosNumeros(int numero1, int numero2)
+    //{
+    //    int sumaDeNumeros;
+    //    for (int i = numero1; i < numero2; i++)
+    //    {
+    //        int sumaDeNumeros = i + i;
+            
+    //    }
+    //    return sumaDeNumeros;
+    //}
+}
