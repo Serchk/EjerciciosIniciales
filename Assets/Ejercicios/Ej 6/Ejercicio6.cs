017c5887705900625805b40ff5f51e4e6e940495
Serchk <sergioperezalcal@gmail.com>
2024-09-29 18:07:26 +0200

    ej 6.1 y 6.2

---
+++ Assets/Ejercicios/Ej 6/Ejercicio6.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio6 : MonoBehaviour
+{
+    // 6.1
+    //a) (X && Y) || (X && Z) = true
+    //b) (X || !Y) && (!X || Z) = true
+    //c) X || Y && Z = true
+    //d) !(X || Y) && Z = false
+    //e) X || Y || X && !Z && !Y =  true
+    //f) !X || !Y || Z && X && !Y = true
+
+    //6.2
+    //a) W || Y && X && W || Z = false
+    //b) X && !Y && !X || !W && Y = true
+    //c) !(W || !Y) && X || Z = false
+    //d) X && Y && W || Z || X = true
+    //e) Y || !(Y || Z && W) = true
+    //f) !X && Y && (!Z || !X) = false
+}
